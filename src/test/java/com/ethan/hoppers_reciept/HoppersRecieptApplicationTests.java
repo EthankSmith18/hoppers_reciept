1dfe2307ec84cd7cd64f93192a9205918a9db6af
Ethan Smith <EthanKSmith@gmail.com>
2023-02-10 13:17:54 -0700

    Initial Commit

---
+++ src/test/java/com/ethan/hoppers_reciept/HoppersRecieptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ethan.hoppers_reciept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersRecieptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
